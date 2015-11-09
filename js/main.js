f6ffab43b152829fc381ed0e38ca88609dc2712f
Tonkoshkuryk Serhii <rapyo@ukr.net>
2015-11-09 14:16:35 +0300

    7 slides

---
+++ js/main.js
@@ -0,0 +1,283 @@
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}(';(8($){$.2G.2H=8(F){6 G={R:"",S:"",1y:0,1z:2I,1e:N,1f:N,1g:N,1h:N,T:10,1i:16,1j:16,1k:16,1l:16,1A:23,1B:23,1C:23,1D:23,1E:23,1F:23,1G:23,1H:23,1I:"#5",1J:"#5",1K:"#5",1L:"#5",1M:"#18",1N:"#18",1O:"#18",1P:"#18",1Q:32,1R:32,1S:32,1T:32,1U:11,1V:11,1W:11,1X:11,1Y:"#19",1Z:"#19",20:"#19",21:"#19",22:"#1a",25:"#1a",26:"#1a",27:"#1a",28:"2J",29:"2K",2a:"2L",2b:"2M",2c:0,2e:0,2f:0,2g:0,U:8(){}};6 F=$.2N({},G,F);1m 7.2O(8(){6 a=$(7);a.2P("2Q-2R");6 b=1n 2h($(7),F);b.2i()});8 2h(t,u){6 v;6 z,$1o,$1p,$1q;6 A,V,1r,W;6 B,d,h,i,s;6 C;6 D=2S.2T("9").1s;6 E=2U(u.U)=="8"?u.U:8(){};7.2i=8(){6 a=7;v=t;C=H.2V(u.1i,u.1j,u.1k,u.1l);u.R=7.1t(u.R);u.S=7.1t(u.S);z=7.X(u.1e,"2W",u.1i,u.1A,u.1E,u.1I,u.1M,u.1Q,u.1U,u.1Y,u.22,u.2c,u.28);$1o=7.X(u.1f,"2X",u.1j,u.1B,u.1F,u.1J,u.1N,u.1R,u.1V,u.1Z,u.25,u.2e,u.29);$1p=7.X(u.1g,"2Y",u.1k,u.1C,u.1G,u.1K,u.1O,u.1S,u.1W,u.20,u.26,u.2f,u.2a);$1q=7.X(u.1h,"2Z",u.1l,u.1D,u.1H,u.1L,u.1P,u.1T,u.1X,u.21,u.27,u.2g,u.2b);v.U=E;7.2j=30(8(){a.1u()},1v);a.1u()};7.1t=8(a){6 b=a.1w("/").2k(" ").1w(":").2k(" ").1w(" ");6 y=O(b[0],10);6 m=O(b[1],10)-1;6 d=O(b[2],10);6 h=O(b[3],10);6 i=O(b[4],10)-u.1y*P;6 s=O(b[5],10);a=1n 2l(y,m,d,h,i,s,0).2m();1m a};7.X=8(a,b,c,d,e,f,g,i,j,k,l,m,n){6 o=31;I(a){6 w,h,Y,Z;6 x,y,r;6 p,$J;o=$(\'<Q 12="\'+b+\' V">								   <9 12="2n"></9>								   <9 12="K"></9>								   <Q 12="1x">0</Q>								   <Q 12="J"></Q>							   </Q>\');w=c;h=!m?c:(c+m+i);o.1b({13:w,L:h,"T":u.T+"M"});o.1c({"2o":c,"33":d,"2p":e,"2q":g});v.34(o);I(c!=C){Z=H.35((C-c)/2);o.1b("T-2r",Z+"M")}p=o.14(".1x");Y=!m?(c-j):c;p.1b({13:c,L:c,"2s-L":Y+"M","2t-2u":i+"M","2v":k});$J=o.14(".J");Y=!m?(c+i):i;Z=!m?0:(c+m);$J.J(n).1b({13:c,L:c,"2s-L":Y+"M","2t-2u":j+"M","2v":l,"T-2r":Z+"M"});I(D){x=y=c/2;r=x-d/2;6 q=o.14("9.2n")[0].1s("2d");q.9.13=c;q.9.L=c;q.2w=d;q.2x=f;q.2y(0,0,d,d);q.2z();q.2A(x,y,r,0,H.1d*2,N);q.2B();q.2C()}}1m o};7.K=8(a,b,c){6 d=a.1c("2o");6 e=a.1c("2p");6 f=a.1c("2q");6 x,y,r;x=y=d/2;r=x-e/2;6 g=a.14(".1x");g.J(b);I(D){6 h=a.14("9.K")[0].1s("2d");h.9.13=d;h.9.L=d;h.2w=e;h.2x=f;h.2y(0,0,e,e);h.2z();h.2A(x,y,r,(H.1d*2*(b/c))-(H.1d/2),-H.1d/2,N);h.2B();h.2C()}};7.1u=8(){V=1n 2l();A=V.2m()+V.36()*P*1v;W=!u.1z?u.S-A:A-u.R;I(W<0){37(7.2j);W=0;v.U.38(7)}1r=u.S-u.R;B=H.15(1r/(39));7.2D(W);I(u.1e){7.K(z,d,B)}I(u.1f){7.K($1o,h,24)}I(u.1g){7.K($1p,i,P)}I(u.1h){7.K($1q,s,P)}};7.2D=8(a){6 b=H.15(a/1v);s=b%P;i=H.15(b%(2E)/P);h=H.15(b%(2F)/2E);d=H.15(b/2F)}}}})(3a);',62,197,'||||||var|this|function|canvas||||||||||||||||||||||||||||||||||Math|if|text|circle|height|px|true|parseInt|60|div|startDate|endDate|margin|onFinish|time|endTimeDiff|addItem|lh|marginTop|||class|width|find|floor|160|ccc|e8065c|333|666|css|data|PI|showDay|showHour|showMinute|showSecond|dayDiameter|hourDiameter|minuteDiameter|secondDiameter|return|new|hours|minutes|seconds|startTimeDiff|getContext|convertToTime|checkTime|1000|split|count|timeZone|past|dayBgWidth|hourBgWidth|minuteBgWidth|secondBgWidth|dayCircleWidth|hourCircleWidth|minuteCircleWidth|secondCircleWidth|dayBgColor|hourBgColor|minuteBgColor|secondBgColor|dayCircleColor|hourCircleColor|minuteCircleColor|secondCircleColor|dayCounterFontSize|hourCounterFontSize|minuteCounterFontSize|secondCounterFontSize|dayTextFontSize|hourTextFontSize|minuteTextFontSize|secondTextFontSize|dayCounterFontColor|hourCounterFontColor|minuteCounterFontColor|secondCounterFontColor|dayTextFontColor|||hourTextFontColor|minuteTextFontColor|secondTextFontColor|dayText|hourText|minuteText|secondText|dayTextMarginTop||hourTextMarginTop|minuteTextMarginTop|secondTextMarginTop|CircularCountDown|init|intervalId|join|Date|getTime|bg|diameter|circleWidth|circleColor|top|line|font|size|color|lineWidth|strokeStyle|clearRect|beginPath|arc|stroke|closePath|timeFormat|3600|86400|fn|circularCountdown|false|ДНЕЙ|ЧАСОВ|МИНУТ|СЕКУНД|extend|each|addClass|circular|countdown|document|createElement|typeof|max|day|hour|minute|second|setInterval|null||bgWidth|append|round|getTimezoneOffset|clearInterval|call|86400000|jQuery'.split('|'),0,{}))
+
+var windowPosition = window.scrollY;
+var htmlWidth	= ($('.body').innerWidth()-15),
+	bodyChildren = $('.slide').length;
+
+$(document).ready(function(){
+	//console.log (htmlWidth)
+	$('.body').css("width", (htmlWidth * bodyChildren)); //установка ширины .body (сумма ширин .slide - ов)
+	$('.slide').css("width", htmlWidth); //установка ширины .slide (по ширине она )
+	
+	$('.scrollLength').css("height", htmlWidth * bodyChildren-(5*bodyChildren)); // установка ленты прокрутки вниз !подправить!
+	//$('.scrollLength').css("height", 21250)
+	$('.htmlWidth').text(htmlWidth); //первоначальный вывод ширины окна
+//Обработка изменения .body, .scrollLength, .slide
+$( window ).resize(function(){
+	htmlWidth = $(window).innerWidth();  //контроль текущей ширины <html>
+	//console.log (htmlWidth); проверка работы
+	console.log ($('.body').innerWidth());
+	$('.body').css("width", (htmlWidth * bodyChildren)); //отслеживание размера при .resize()
+	$('.slide').css("width", htmlWidth);                 //отслеживание размера при .resize()
+	$('.scrollLength').css("height", htmlWidth * bodyChildren);              //отслеживание размера при .resize()
+	$('.htmlWidth').text('ширина html =' + htmlWidth); 
+});
+
+
+
+// создать в .scrollLength div-ы в по количеству слайдов
+// и с такими же id, в которых высота равна 
+//ширине слайдов, тоесть htmlWidth
+	
+/*console.log ('bodyChildren =' + bodyChildren);
+for (var i = 0; i < bodyChildren.length; i++) {
+	$('.scrollLength').append('<div>');
+};*/
+	
+	
+});
+
+
+//обработка скорости и направления перемещения кадров
+$(window).scroll( function (e){
+	var windowPosition = window.scrollY,
+		positionBody = -(window.scrollY*1),
+		position1 = window.scrollY*0.08,
+		position2 = window.scrollY*0.3;
+
+	//$('.body').text("position" + window.scrollY);
+	$('.body').css({
+		'height': '100%',
+		'overflow-x': 'visible',
+		'position': 'fixed',
+		'left':'0px',
+		'transform': 'translate3d(' + positionBody + 'px, 0px,0px)'
+		});
+//	$('.body').css('margin-left', -positionBody); //прокрутка ленты
+	$('.background').css('margin-left', positionBody); //прокрутка ленты
+	
+	//прокрутка кадров 1-го слайда (скорость и направление)
+	$('.frame1').css({'left' : (7+(-windowPosition*0.0059))+'%'}); 
+	$('.frame2').css({'left' : (77+(-position1*0.02))+'%'});
+	$('.frame3').css({'left' : (15+(windowPosition*0.025))+'%'});
+
+	//прокрутка кадров 2-го слайда
+	$('.frame4').css({'left' : (55+(-(windowPosition)*0.043))+'%'}); 
+	$('.frame5').css({'left' : (8+(windowPosition*0.011))+'%'});
+	$('.frame6').css({'left' : (29+(-windowPosition*0.01))+'%'});
+	$('.frame6-1').css({'left' : (100+(-windowPosition*0.02))+'%'});
+
+
+	//прокрутка кадров 3-го слайда
+	$('.frame7').css({'left' : (8+(windowPosition*0.010))+'%'});
+	$('.frame8').css({'top' : (-40+(windowPosition*0.025))+'%'});
+	
+
+
+	//прокрутка кадров 4-го слайда
+	$('.frame9').css({'left' : (-25+(windowPosition*0.015))+'%'});
+	$('.frame10').css({'top' : (-80+(windowPosition*0.025))+'%'}); 
+	
+
+	//прокрутка кадров 5-го слайда
+	$('.frame11').css({'left' : (-55+(windowPosition*0.015))+'%'});
+	$('.frame12').css({'top' : (-140+(windowPosition*0.030))+'%'});
+	
+	//прокрутка кадров 6-го слайда
+	//$('.frame13').css('left', -50+((windowPosition-2500)*0.1)); 
+	$('.frame14').css({'left' : (-60+(windowPosition*0.010))+'%'});
+	//$('.frame15').css('right', -30+((windowPosition-2500)*0.1));
+
+	//прокрутка кадров 7-го слайда
+	//$('.frame4').css('left', -50+((windowPosition-2500)*0.1)); 
+	//$('.frame5').css('left', 40+ ((windowPosition-2500)*0.1));
+	//$('.frame6').css('right', -30+((windowPosition-2500)*0.1));
+
+
+
+	
+	//вывод информации о позиции окна
+/*	$('.chekpoint').text(windowPosition);
+	$('.htmlWidth').text('ширина html =' + htmlWidth);
+*/
+});
+
+
+
+
+// обработка кнопок меню для прокрутки по слайдам
+/*var scrollSprrd = 1200 // скорость прокрутки слайдов
+$('#link1').click(function(){
+	$("body").animate({"scrollTop": 0}, scrollSprrd);
+});
+$('#link2').on('click', function(){
+	$("body").animate({"scrollTop": htmlWidth}, scrollSprrd);
+});
+$('#link3').on('click', function(){
+	$("body").animate({"scrollTop": htmlWidth*2}, scrollSprrd);
+});
+$('#link4').on('click', function(){
+	$("body").animate({"scrollTop": htmlWidth*3}, scrollSprrd);
+});
+$('#link5').on('click', function(){
+	$("body").animate({"scrollTop": htmlWidth*4}, scrollSprrd);
+});
+$('#link6').on('click', function(){
+	$("body").animate({"scrollTop": htmlWidth*5}, scrollSprrd);
+});
+$('#link7').on('click', function(){
+	$("body").animate({"scrollTop": htmlWidth*6}, scrollSprrd);
+});*/
+
+
+
+$(document).ready(function() {
+				$(".countdown").circularCountdown({
+					startDate:"2014/08/01 12:30:00",
+					endDate:"2016/12/25 12:30:00",
+					timeZone:+3,	//Time zone of New York. Find timezone of your location and write here.
+					
+					//Margin between circles
+					margin:15,
+					
+					//Diameters
+					dayDiameter:160,
+					hourDiameter:160,
+					minuteDiameter:160,
+					secondDiameter:160,
+					
+					//Circle BG width
+					dayBgWidth:5,
+					hourBgWidth:5,
+					minuteBgWidth:5,
+					secondBgWidth:5,
+					
+					//Circle width
+					dayCircleWidth:5,
+					hourCircleWidth:5,
+					minuteCircleWidth:5,
+					secondCircleWidth:5,
+					
+					//Circle color
+					dayCircleColor:"#ff6565",
+					hourCircleColor:"#378cff",
+					minuteCircleColor:"#9cdb7d",
+					secondCircleColor:"#ffdc50",
+					
+					//Counter font size
+					dayCounterFontSize:36,
+					hourCounterFontSize:36,
+					minuteCounterFontSize:36,
+					secondCounterFontSize:36,
+					
+					//Text font size
+					dayTextFontSize:12,
+					hourTextFontSize:12,
+					minuteTextFontSize:12,
+					secondTextFontSize:12,
+					
+					//Counter font color
+					dayCounterFontColor:"#fff",
+					hourCounterFontColor:"#fff",
+					minuteCounterFontColor:"#fff",
+					secondCounterFontColor:"#fff",
+					
+					//Text font color
+					dayTextFontColor:"#ff6565",
+					hourTextFontColor:"#378cff",
+					minuteTextFontColor:"#9cdb7d",
+					secondTextFontColor:"#ffdc50"
+				});
+			});
+
+window.onload = function () {
+    'use strict';
+
+    var view, indicator, relative,
+        min, max, offset, reference, pressed, xform,
+        velocity, frame, timestamp, ticker,vamplitude, target, timeConstant;
+   
+   function ypos(e) {
+        // touch event
+        if (e.targetTouches && (e.targetTouches.length >= 1)) {
+            return e.targetTouches[0].clientY;
+        }
+
+        // mouse event
+        return e.clientY;
+    }
+
+    function scroll(y) {
+        offset = (y > max) ? max : (y < min) ? min : y;
+        view.style[xform] = 'translateY(' + (-offset) + 'px)';
+    }
+
+    view = document.getElementsByClassName('body')[0];
+
+    function track() {
+    var now, elapsed, delta, v;
+ 
+    now = Date.now();
+    elapsed = now - timestamp;
+    timestamp = now;
+    delta = offset - frame;
+    frame = offset;
+ 
+    v = 1000 * delta / (1 + elapsed);
+    velocity = 0.8 * v + 0.2 * velocity;
+	}
+
+/*                           */
+	
+	function release(e) {
+    pressed = false;
+ 
+    clearInterval(ticker);
+    if (velocity > 10 || velocity < -10) {
+        amplitude = 0.8 * velocity;
+        target = Math.round(offset + amplitude);
+        timestamp = Date.now();
+        requestAnimationFrame(autoScroll);
+    }
+ 
+    e.preventDefault();
+    e.stopPropagation();
+    return false;
+	}
+
+/*                               */
+
+	function autoScroll() {
+	    var elapsed, delta;
+	    if (amplitude) {
+	        elapsed = Date.now() - timestamp;
+	        delta = -amplitude * Math.exp(-elapsed / timeConstant);
+	        if (delta > 0.5 || delta < -0.5) {
+	            scroll(target + delta);
+	            requestAnimationFrame(autoScroll);
+	        } else {
+	            scroll(target);
+	        }
+	    }
+	}
+
+
+/*                              */
+
+    max = parseInt(getComputedStyle(view).width, 10) - innerHeight;
+    offset = min = 0;
+    pressed = false;
+    timeConstant = 325; // ms
+
+ xform = 'transform';
+    ['webkit', 'Moz', 'O', 'ms'].every(function (prefix) {
+        var e = prefix + 'Transform';
+        if (typeof view.style[e] !== 'undefined') {
+            xform = e;
+            return false;
+        }
+        return true;
+    });
+            console.log(xform);
+
+};
